3b8ea441f0c52ce19f8890c2e5183084c1dcc6a8
Philéas Samir <phileas.samir@gmail.com>
2020-02-20 11:45:44 +0100

    Update detect_video.py

--- detect_video.py
+++ detect_video.py
@@ -81,7 +81,7 @@ def main(_argv):
         if FLAGS.output:
             out.write(img)
         clear_output(wait=True)
-        display(Image(cv2_imshow(img)))
+        display(Image(data=bytes(cv2.imencode('.jpg', img)[1]), width=800))
         if cv2.waitKey(1) == ord('q'):
             break
 
